e1caf708cec9bba8d5e2985741b599cec1869065
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2022-09-12 11:33:13 -0500

    my first page

--- dia-5/main.js
+++ dia-5/main.js
@@ -1,1 +1,28 @@
 // voy a decirle a la persona cuanto impuesto pagar, dependiento su monto de renta anual.
+
+/* 
+  Menos de 10000€ 	5%
+  Entre 10000€ y 20000€ 	15%
+  Entre 20000€ y 35000€ 	20%
+  Entre 35000€ y 60000€ 	30%
+  Más de 60000€ 	45%
+*/
+
+let edad = parseInt(prompt("Cuantos años tienes?"));
+
+// let resultado = " ";
+
+// let resultado = condicion ? true : false;
+
+let resultado = edad < 18 ? 'No puede pasar' : 'Adelante Señor';
+
+
+
+// if (edad < 18) {
+//   resultado = "no puedes pasar";
+// } else {
+//   resultado = 'Adelante señor';
+// }
+
+alert(resultado);
+// let renta = 10000;
---
+++ dia-6/day-5.js
@@ -0,0 +1,38 @@
+// Los alumnos de un curso se han dividido en dos grupos A y B
+//de acuerdo al sexo y el nombre. El grupo A esta formado por 
+// las mujeres con un nombre anterior a la M y los hombres con 
+// un nombre posterior a la N y el grupo B por el resto. 
+// Escribir un programa que pregunte al usuario su nombre y 
+//sexo, y muestre por pantalla el grupo que le corresponde.
+
+// Mejora tu programa tanto como el tiempo te lo permita.
+//Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado*/
+
+
+let sexo = prompt("Ingresa tu sexo. (H si eres Hombre o M si eres Mujer)");
+
+while (sexo.toUpperCase() != 'H' && sexo.toUpperCase() != 'M') {
+  alert("Lo que ingresaste no es un sexo valido. intentalo nuevamente");
+  sexo = prompt("Ingresa tu sexo. (H si eres Hombre o M si eres Mujer)");
+}
+
+let nombre = prompt("Ingresa tu nombre.");
+let isMujerInA = 'ABCDEFGHIJKL';
+let isHombreInA = 'OPQRSTUVWXYZ';
+let isInA = false;
+
+
+if(sexo == 'M' && isMujerInA.search(nombre[0]) >= 0) { // Mujer
+  isInA = true;
+} else if(sexo = 'H' && isHombreInA.search(nombre[0]) >= 0) {
+  isInA = true;
+}// Hombre
+
+if(isInA) {
+  alert("Perteneces al grupo A")
+} else {
+  alert("Perteneces al grupo B")
+}
+
+// nombre[0] esta en abecedario(las primeras 12 letas)
+// Search == variable.search('queestasBuscando');
